fef0da028d0864bcaa95ea7def2e8a4e4c494e99
Daviisq <davi.domingos@etec.sp.gov.br>
2024-09-13 13:55:41 -0300

    adicionando projeto completo 13-09(13:55)

---
+++ Teste/Funcionario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    internal class Funcionario
+    {
+        protected internal int idFunc;
+        protected internal string senhaFunc;
+        protected internal double salario;
+        protected internal string turno;
+        protected internal string tipoContrato;
+    }
+}
---
+++ Teste/Gerente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    internal class Gerente
+    {
+        internal int idGere;
+        internal string senhaGere = "0000";
+        internal string setor;
+
+        internal void cadastrarGerente()
+        {
+
+        }
+        internal void cadastrarFunc()
+        {
+
+        }
+    }
+}
